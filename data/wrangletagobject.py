ace2c7b35a1b5990e2ec28445b99d6c6e6ba039f
Isabelle Zheng <isa.zheng@gmail.com>
2019-11-17 18:31:44 -0500

    Basic data wrangling and visualization for bar chart

---
+++ data/wrangletagobject.py
@@ -0,0 +1,44 @@
+import json
+
+tag_dict = {}
+tag_dict["total"] = {}
+
+tag_list = {}
+tag_list["total"] = []
+
+with open("book_data.json") as json_file:
+  data = json.load(json_file)
+  for book in data:
+    if int(book["book_id"]) % 1000 == 0:
+      print("BOOK: " + book["book_id"])
+    if "localizedObjectAnnotations" in book.keys():
+      for tag in book["tags"]:
+        tag_name = tag["tag_name"]
+        if tag_name in tag_dict.keys():
+          for obj in book["localizedObjectAnnotations"]:
+            if obj["name"] in tag_dict[tag_name].keys():
+             tag_dict[tag_name][obj["name"]] += obj["score"]
+            else:
+              tag_dict[tag_name][obj["name"]] = obj["score"]
+        else:
+          tag_dict[tag_name] = {}
+          for obj in book["localizedObjectAnnotations"]:
+            tag_dict[tag_name][obj["name"]] = obj["score"]
+      for obj in book["localizedObjectAnnotations"]:
+        if obj["name"] in tag_dict["total"].keys():
+         tag_dict["total"][obj["name"]] += obj["score"]
+        else:
+          tag_dict["total"][obj["name"]] = obj["score"]
+
+
+
+for tag_name, tag_entry in tag_dict.items():
+    tag_list[tag_name] = []
+    for obj_name, score in tag_entry.items():
+        tag_list[tag_name].append({"name": obj_name, "score": score})
+
+
+with open('tag_object.json', 'w') as file:
+  json.dump(tag_list, file, ensure_ascii=False, indent=4)
+
+
---
+++ js/bar-chart.js
@@ -0,0 +1,135 @@
+
+/*
+ * PrioVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data						-- the actual data: perDayData
+ */
+
+BarChart = function(_parentElement, _data, _metaData){
+    this.parentElement = _parentElement;
+    this.data = _data;
+
+    this.initVis();
+}
+
+
+/*
+ * Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+BarChart.prototype.initVis = function(){
+    var vis = this;
+
+    vis.margin = { top: 20, right: 60, bottom: 20, left: 100 };
+
+    vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right,
+        vis.height = 800 - vis.margin.top - vis.margin.bottom;
+
+    // SVG drawing area
+    vis.svg = d3.select("#" + vis.parentElement).append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+    // Scales and axes
+    vis.x = d3.scaleLinear()
+        .range([0,vis.width]);
+
+    vis.y = d3.scaleBand()
+        .rangeRound([0, vis.height])
+        .paddingInner(0.2);
+
+    // vis.xAxis = d3.axisBottom()
+    //     .scale(vis.x);
+
+    vis.yAxis = d3.axisLeft()
+        .scale(vis.y);
+
+    // vis.svg.append("g")
+    //     .attr("class", "x-axis axis")
+    //     .attr("transform", "translate(0," + vis.height + ")");
+
+
+    vis.svg.append("g")
+        .attr("class", "y-axis axis");
+
+    // (Filter, aggregate, modify data)
+    vis.wrangleData();
+}
+
+
+/*
+ * Data wrangling
+ */
+
+BarChart.prototype.wrangleData = function(){
+    var vis = this;
+
+    vis.displayData = vis.data["total"];
+
+    vis.displayData.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    vis.displayData = vis.displayData.slice(0, 20);
+
+    // Update the visualization
+    vis.updateVis();
+}
+
+
+/*
+ * The drawing function
+ */
+
+BarChart.prototype.updateVis = function(){
+    var vis = this;
+
+    // Update domains
+    vis.x.domain([0, d3.max(vis.displayData, function(d) {
+        return d.score;
+    })])
+
+    vis.y.domain(vis.displayData.map(function(d) {
+        return d.name;
+    }));
+
+    var bars = vis.svg.selectAll(".bar")
+        .data(vis.displayData);
+
+    bars.enter().append("rect")
+        .attr("class", "bar")
+
+        .merge(bars)
+        .transition()
+        .attr("width", function(d) { return vis.x(d.score); })
+        .attr("height", vis.y.bandwidth())
+        .attr("x", function(d){
+            return vis.x(0);
+        })
+        .attr("y", function(d){
+            return vis.y(d.name);
+        })
+        .attr("fill", "black")
+
+    // vis.svg.select(".x-axis").call(vis.xAxis);
+    vis.svg.select(".y-axis").call(vis.yAxis);
+
+    bars.enter().append("text")
+        .attr("class", "data-label")
+
+        .merge(bars)
+        .transition()
+        .attr("x", function(d){
+            return vis.x(d.score) + 10;
+        })
+        .attr("y", function(d){
+            return vis.y(d.name) + vis.y.bandwidth()/2 + 6;
+        })
+        .text(function(d) {
+            return Math.round(d.score);
+        })
+        .attr("fill", "black");
+}
+
