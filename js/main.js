1bf9e4d256e6ee1bda3491a24baf7ea287ca51e2
lekshmischool1789 <lekshmischool1789@gmail.com>
2019-11-17 14:18:53 -0800

    updated color image placeholder

--- js/main.js
+++ js/main.js
@@ -6,6 +6,6 @@ d3.json("data/book-data-lite.json", function(data)
 {
 
     vis.data = data;
-    innovativeview = new InnovativeView("playground-vis-img", data);
+    innovativeview = new InnovativeView("color-vis", data);
 
 });
