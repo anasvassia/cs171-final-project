839e4a06ffdfb3d3257bd05cec9e9c5310c88c18
anasvassia <alamothe@college.harvard.edu>
2019-11-24 01:51:18 -0500

    User interaction for book selection
    User interaction for book selection; progress made on website layout (not yet complete)

--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,24 @@
 
+var books;
+var alreadyChose = false;
+
+// selection is the isbn of the book that the user chose
+var selection;
+
+// load data
 d3.json("data/book-data-lite.json", function(data) {
     var genreArray = [
         "fantasy", "science-fiction", "nonfiction", "young-adult",
         "childrens", "horror", "romance", "mystery"
     ];
 
-    var books = d3.range(0, 8).map( function(val) {
+    // book covers to be displayed
+   books = d3.range(0, 8).map( function(val) {
         return random(genreArray[val]);
 
         });
 
+    // generate new books on each page load
     function random(genre) {
         var result = data[Math.floor(Math.random()*data.length)];
         if (result.tags.includes(genre) && !(result.image_url.includes("nophoto"))) {
@@ -20,20 +29,24 @@ d3.json("data/book-data-lite.json", function(data) {
         }
     }
 
+    // set images and on click listener
     $.each(books, function(index, val) {
         var select = "#img"+index;
         $(select)
             .attr("src", val.image_url)
             .attr("alt", val.title);
+
+        $(select).on("click", function() {
+            // if user has already chosen something then don't do anything
+            if (alreadyChose) {
+                return;
+            }
+
+            // else let user choose a book
+            $("#choice-explanation").text(val.title);
+            alreadyChose = true;
+        });
     });
-
-
-
-    console.log(books);
-
-
-    console.log(data);
 });
-
 
 
